9aa64ba6ee92c54165f948618009feaa4007c759
Rahilsamani <rahilahmed1720@gmail.com>
2024-07-03 23:13:40 +0530

    add cors

--- server/index.js
+++ server/index.js
@@ -16,7 +16,14 @@ database.connect();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 app.use(
   fileUpload({
